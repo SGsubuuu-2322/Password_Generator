ae8992d57f194555836d710033338fd16d996cf2
Subham Pradhan <59175226+SGsubuuu-2322@users.noreply.github.com>
2023-12-23 21:09:00 +0530

    Debugged some scripts...

--- script.js
+++ script.js
@@ -72,11 +72,11 @@ function generatePassword(length, upper, lower, number, symbol) {
   return finalPassword;
 }
 
-function getRandomLowerCase() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
 function getRandomUpperCase() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomLowerCase() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
 function getRandomNumber() {
   return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
